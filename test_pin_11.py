38aa9554ac1512d4e58379a450086f04777eb3eb
ra <rayax86@gmail.com>
2017-05-16 20:54:33 +1000

    updated test_pin_11.py

--- test_pin_11.py
+++ test_pin_11.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as gpio
 from time import sleep
+from datetime import datetime as dt
 
 gpio.setmode(gpio.BOARD)
 gpio.setup(11, gpio.IN)
 
 try:
 	while True:
-		print(gpio.input(11))
-		sleep(1)
+		print("[{0}] {1}".format(dt.now, gpio.input(11)))
+		sleep(0.5)
 except KeyboardInterrupt:
 	pass
 
+print("\n")
 print("Gracefully exiting")
 gpio.cleanup()
