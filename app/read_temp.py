46385f30645455db9609441a79399aac8c6584c7
ra <rayax86@gmail.com>
2017-05-18 21:11:51 +1000

    .

--- app/read_temp.py
+++ app/read_temp.py
@@ -1,6 +1,9 @@
 from digitemp.master import UART_Adapter
 from digitemp.device import DS18B20
 
+server = "52.65.244.105"
+api = "/api/temperature"
+
 bus = UART_Adapter('/dev/serial0')  # DS9097 connected to COM1
 
 # only one 1-wire device on the bus:
@@ -8,5 +11,17 @@ sensor = DS18B20(bus)
 
 sensor.info()
 
+temp = sensor.get_temperature()
+
 # get temperature
-print(sensor.get_temperature())
+print(temp)
+
+conn = http.HTTPConnection(server)
+	headers = {'Content-type': 'application/json'}
+try:
+	conn.request("POST", api, json.dumps({'temperature': temp}), headers)
+	print("report to {0} successful".format(server))
+except:
+	print("report to {0} failed".format(server))
+finally:
+	conn.close()
