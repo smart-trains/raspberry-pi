865238e850aa1723d7e07ae9e9aa0049651a5d8b
ra <rayax86@gmail.com>
2017-07-10 14:07:20 +1000

    fixed

--- app/test_rs485.py
+++ app/test_rs485.py
@@ -4,10 +4,6 @@ from datetime import datetime as dt
 
 # helper type
 class Const(object):
- 	"""docstring for Const"""
- 	def __init__(self, arg):
- 		raise TypeError("The Const type is not allowed to be instantiated.")
-
  	_RO = 31
  	_RE = 33
  	_DE = 35
@@ -26,19 +22,21 @@ class Const(object):
  	def PIN_DI(self):
  		return type(self)._DI
 
+c = Const()
+
 # Borad setup
 gpio.setmode(gpio.BOARD)
-gpio.setup(Const.PIN_RO, gpio.IN, gpio.PUD_UP)
-gpio.setup(Const.PIN_RE, gpio.OUT, gpio.PUD_UP);
-gpio.setup(Const.PIN_DE, gpio.OUT, gpio.PUD_DOWN);
-gpio.setup(Const.PIN_DI, gpio.OUT, gpio.PUD_UP);
+gpio.setup(c.PIN_RO, gpio.IN, gpio.PUD_UP)
+gpio.setup(c.PIN_RE, gpio.OUT, gpio.PUD_UP);
+gpio.setup(c.PIN_DE, gpio.OUT, gpio.PUD_DOWN);
+gpio.setup(c.PIN_DI, gpio.OUT, gpio.PUD_UP);
 
-gpio.output(Const.PIN_DE, 1)
+gpio.output(c.PIN_DE, 1)
 
 # Program
 try:
 	while True:
-		gpio.output(Const.PIN_DI, dt.datetime.now().second % 2)
+		gpio.output(c.PIN_DI, dt.datetime.now().second % 2)
 		sleep(1)
 except KeyboardInterrupt:
 	pass
