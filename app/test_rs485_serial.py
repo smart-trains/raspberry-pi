5ef35db0889b81dd73640166006f539c9d5f2f03
ra <rayax86@gmail.com>
2017-07-19 14:28:03 +1000

    ..

--- app/test_rs485_serial.py
+++ app/test_rs485_serial.py
@@ -2,16 +2,19 @@ import serial
 from time import sleep
 from datetime import datetime as dt
 
-s = serial.Serial(port="/dev/ttyAMA0")
+s = serial.Serial(
+		port="/dev/ttyAMA0",
+		bytesize=serial.EIGHTBITS
+	)
 
 # Program
 try:
 	while True:
-		s.write("caosi pangpang")
+		s.write(b"caosi pangpang")
 		sleep(1)
 except KeyboardInterrupt:
 	pass
 
 print("\n")
 print("Gracefully exiting")
-serial.close()
+serial.Serial.close()
